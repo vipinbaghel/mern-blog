b7198d97759034886dd0db74095b5063021a1903
vipin <vipin10082001@gmail.com>
2023-07-02 22:51:10 +0530

    changed the start script

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use('', userRouter);
 app.use('', postRouter);
 
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> {
     console.log(`server is running on port ${PORT}`);
 });
